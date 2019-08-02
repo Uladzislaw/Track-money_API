9a2e239decb9c941f04ea2cf37ce063e3a0dca1e
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-02 11:06:20 +0300

    Add user repo

---
+++ src/main/java/com/serh/trackmoney/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.serh.trackmoney.repository;
+
+import com.serh.trackmoney.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
