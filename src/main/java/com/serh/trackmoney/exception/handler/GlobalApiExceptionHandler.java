8aad9a23e2545f3ec96d6941b7d0393dfddfedc2
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-04 12:47:20 +0300

    Add exception handle for valid fields

--- src/main/java/com/serh/trackmoney/exception/handler/GlobalApiExceptionHandler.java
+++ src/main/java/com/serh/trackmoney/exception/handler/GlobalApiExceptionHandler.java
@@ -2,13 +2,22 @@ package com.serh.trackmoney.exception.handler;
 
 import com.serh.trackmoney.exception.ApiErrorResponse;
 import com.serh.trackmoney.exception.api.UserAlreadyExistsException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -21,5 +30,27 @@ public class GlobalApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException ex,
+            final HttpHeaders headers,
+            final HttpStatus status, final WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+
     }
 }
