df07cb6b7477fc7c1322effda206a2cb98202fd4
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-04 12:39:46 +0300

    Add method to find by email

--- src/main/java/com/serh/trackmoney/repository/UserRepository.java
+++ src/main/java/com/serh/trackmoney/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.serh.trackmoney.repository;
 import com.serh.trackmoney.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findOneByEmail(String email);
+
     boolean existsByEmail(String email);
 }
