66ad99ad2756b8cc820388f0d320f7a667728098
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-04 12:39:19 +0300

    Enable jpa repo

--- src/main/java/com/serh/trackmoney/TrackMoneyApplication.java
+++ src/main/java/com/serh/trackmoney/TrackMoneyApplication.java
@@ -2,8 +2,10 @@ package com.serh.trackmoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class TrackMoneyApplication {
 
     public static void main(String[] args) {
