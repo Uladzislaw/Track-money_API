845d73bd80bda4683c4c5400e64610d45aebf8fd
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-10 14:06:02 +0300

    Delete annotation validation

--- src/main/java/com/serh/trackmoney/dto/UserDto.java
+++ src/main/java/com/serh/trackmoney/dto/UserDto.java
@@ -1,26 +1,21 @@
 package com.serh.trackmoney.dto;
 
-import com.serh.trackmoney.dto.annotation.PasswordMatches;
-import com.serh.trackmoney.dto.annotation.ValidEmail;
 import com.serh.trackmoney.model.Currency;
 import com.serh.trackmoney.model.Role;
 import com.serh.trackmoney.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
-@PasswordMatches
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserDto implements Convertable<User, UserDto>, EntityDto {
 
-    @NonNull
-    @ValidEmail
     private String email;
 
-    @NonNull
     private String password;
     private String matchingPassword;
 
