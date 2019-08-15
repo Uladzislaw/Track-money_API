e76ae15da9228e3ad1ae68e34958bcb59edccd0e
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-15 12:27:04 +0300

    Add mandatory annotations

--- src/main/java/com/serh/trackmoney/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/serh/trackmoney/service/impl/CategoryServiceImpl.java
@@ -1,29 +1,36 @@
 package com.serh.trackmoney.service.impl;
 
-import com.serh.trackmoney.exception.api.UserAlreadyExistsException;
 import com.serh.trackmoney.model.Category;
 import com.serh.trackmoney.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
     @Override
+    @Transactional(readOnly = true)
     public Optional<Category> findOneById(final Long id) {
         return Optional.empty();
     }
 
     @Override
-    public Category update(final Category entity) {
+    public Category update(final Category category) {
         return null;
     }
 
     @Override
-    public Category save(final Category entity) throws UserAlreadyExistsException {
+    public Category save(final Category category) {
         return null;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return null;
     }
