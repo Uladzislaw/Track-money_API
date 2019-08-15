80933b1feda88cfb299db24b0475ceb642253a4b
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-15 13:03:17 +0300

    Add temporary get method

--- src/main/java/com/serh/trackmoney/controller/CategoryController.java
+++ src/main/java/com/serh/trackmoney/controller/CategoryController.java
@@ -1,6 +1,11 @@
 package com.serh.trackmoney.controller;
 
+import com.serh.trackmoney.model.Category;
+import com.serh.trackmoney.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping(value = "/{id}")
+    public Resource<Category> getOne(@PathVariable final Long id) {
+        return new Resource<>(categoryService.findOneById(id).orElseThrow());
+    }
 
 }
