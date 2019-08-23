67785482c77383a7979575a4bddc1b84efb5fdf3
Uladzislaw <vladislav.sergienya@gmail.com>
2019-08-23 14:34:38 +0300

    feat: refactor

--- src/main/java/com/serh/trackmoney/TrackMoneyApplication.java
+++ src/main/java/com/serh/trackmoney/TrackMoneyApplication.java
@@ -1,14 +1,15 @@
 package com.serh.trackmoney;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class TrackMoneyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TrackMoneyApplication.class, args);
+    public static void main(final String[] args) {
+        run(TrackMoneyApplication.class, args);
     }
 }
