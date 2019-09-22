430fe3ad8c9fa05169e8c532576a8c492f17a9bb
Uladzislau <vladislav.sergienya@gmail.com>
2019-09-22 12:05:56 +0300

    Add field to category shows standard categories

--- src/main/resources/db/migration/V1.3__fill_categories.sql
+++ src/main/resources/db/migration/V1.3__fill_categories.sql
@@ -1,13 +1,13 @@
 use trackmoneydb;
 
-insert into categories (name, type)
-values ('Food', 'CONSUMPTION'),
-       ('Healthy', 'CONSUMPTION'),
-       ('Family', 'CONSUMPTION'),
-       ('Cafe', 'CONSUMPTION'),
-       ('Transport', 'CONSUMPTION'),
-       ('Leisure', 'CONSUMPTION'),
-       ('Gifts', 'CONSUMPTION'),
-       ('Clothes', 'CONSUMPTION'),
-       ('Salary', 'INCOME');
+insert into categories (name, type, is_standard)
+values ('Food', 'CONSUMPTION', '1'),
+       ('Healthy', 'CONSUMPTION', '1'),
+       ('Family', 'CONSUMPTION', '1'),
+       ('Cafe', 'CONSUMPTION', '1'),
+       ('Transport', 'CONSUMPTION', '1'),
+       ('Leisure', 'CONSUMPTION', '1'),
+       ('Gifts', 'CONSUMPTION', '1'),
+       ('Clothes', 'CONSUMPTION', '1'),
+       ('Salary', 'INCOME', '1');
 
